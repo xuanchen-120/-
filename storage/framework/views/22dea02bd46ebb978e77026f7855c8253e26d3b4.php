6f3d80b6dd9a587fb04a7d57197b7849eab46f4d
root <122383162@qq.com>
2020-10-15 09:09:26 +0800

    微调

--- storage/framework/views/22dea02bd46ebb978e77026f7855c8253e26d3b4.php
+++
@@ -1,4 +0,0 @@
- <nav class="nav cn_nav" data-display=""  data-show-single="true" data-active-class="active" data-animate="false" style="margin-bottom: 0">
-        <span data-href="<?php echo e(route('chant.index')); ?>"  <?php if(url()->current()==route('chant.index')): ?> class="active" <?php endif; ?> >诵经</span>
-        <span data-href="<?php echo e(route('write.index')); ?>"  <?php if(url()->current()==route('write.index')): ?> class="active" <?php endif; ?> >抄经</span>
-    </nav>
